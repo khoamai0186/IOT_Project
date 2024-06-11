a786fcbcefae01c74f2f93e2080833b3fd297e80
khoamai0186 <lymondrap03@gmail.com>
2024-06-11 16:30:10 +0700

    update2

--- Hardware_Test.py
+++ Hardware_Test.py
@@ -25,4 +25,41 @@ time.sleep(3)
 m485.modbus485_send(relay2_OFF)
 time.sleep(3)
 
+def serial_read_data(ser):
+    bytesToRead = ser.inWaiting()
+    if bytesToRead > 0:
+        out = ser.read(bytesToRead)
+        data_array = [b for b in out]
+        print(data_array)
+        if len(data_array) >= 7:
+            array_size = len(data_array)
+            value = data_array[array_size - 4] * 256 + data_array[array_size - 3]
+            return value
+        else:
+            return -1
+    return 0
+
+
+soil_temperature =[10, 3, 0, 6, 0, 1, 101, 112]
+def readTemperature():
+    serial_read_data(ser)
+    ser.write(soil_temperature)
+    time.sleep(1)
+    return serial_read_data(ser)
+
+soil_moisture = [10, 3, 0, 7, 0, 1, 52, 176]
+def readMoisture():
+    serial_read_data(ser)
+    ser.write(soil_moisture)
+    time.sleep(1)
+    return serial_read_data(ser)
+
+
+print("TEST SENSOR")
+print(readMoisture())
+time.sleep(1)
+print(readTemperature())
+time.sleep(1)
+
 m485.modbus485_read()
+
